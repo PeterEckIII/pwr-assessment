81106bbedd03d5bb8f315e6671ee8cf519bcd163
Peter Eck <jpeckiii@gmail.com>
2019-12-16 16:30:25 -0600

    Add ReviewList component to render reviews

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import ReviewList from './components/ReviewList/ReviewList';
 
-export default () => <div>Review Display</div>;
+const App = () => {
+    const [ reviews, setReviews ] = useState([]);
+
+    const fetchReviews = async () => {
+        const result = await axios.get('/reviews');
+        setReviews(result.data.reviews);
+    }
+
+    useEffect(() => {    
+        fetchReviews()
+    }, [])
+
+    return (
+        <div>
+            <ReviewList reviews={reviews} />
+        </div>
+    )
+}
+
+export default App;
