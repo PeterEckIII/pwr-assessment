d59fe49c1a63b387c016735d862c0c44bdb64e57
Peter Eck <jpeckiii@gmail.com>
2019-12-16 16:31:41 -0600

    Add ReviewList component and styling

---
+++ src/components/ReviewList/ReviewList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Review from '../Review/Review';
+import styles from './ReviewList.module.css';
+
+const ReviewList = ({ reviews }) => {
+    return (
+        <div>
+            <div className={styles.title}>
+                <span>Review Display</span>
+            </div>
+            <ul>
+                { reviews.map(review => {
+                    return (
+                        <Review 
+                            key={review.review_id} 
+                            data-id={review.review_id}
+                            review={ review }
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default ReviewList;
