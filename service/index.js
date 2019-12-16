b9b0318712a256810450730c24b073bf268913f4
Peter Eck <jpeckiii@gmail.com>
2019-12-16 16:28:34 -0600

    Add cors handling

--- service/index.js
+++ service/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const data = require("./reviews");
+const cors = require('cors');
 const PORT = 5000;
+
+app.use(cors());
 
 app.get("/reviews", (req, res) => res.json(data));
 
