5abe8b9946ad6e6e71e01de2feebee04f64bed22
Peter Eck <jpeckiii@gmail.com>
2019-12-16 18:12:28 -0600

    Replace divs with semantic elements

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import styles from './Review.module.css';
 import StarRating from '../star-rating';
-import { relativeTime } from '../../relative-time';
-import classes from './Review.module.css';
+import ReviewAside from './ReviewAside/ReviewAside';
 
 const Review = ({ review }) => (
-    <div className={styles.container} >
-        <div className={styles.star_rating}>
-            <StarRating
-                rating={review.metrics.rating}
-            />
-        </div>
-        <div className={styles.review_headline}>
-            <strong>{review.details.headline}</strong>
-        </div>
-        <div>
-            <div className={styles.review_comment}>
-                {review.details.comments}
+    <article className={ styles.container } >
+        <header>
+            <div className={ styles.star_rating }>
+                <StarRating
+                    rating={ review.metrics.rating }
+                />
             </div>
-        </div>
-        <div className={classes.information}>
-            <div>
-                <span>Submitted: {relativeTime(review.details.created_date)}</span>
+            <div className={ styles.review_headline }>
+                <strong>{ review.details.headline }</strong>
             </div>
-            <div>
-                <span>From:  </span>
-                <span className='review-location'>
-                    {review.details.location}
-                </span>
+        </header>
+        <section class={ styles.review_comment }>
+            <div className={ styles.review_comment }>
+                { review.details.comments }
             </div>
-            <br></br>
-            <div>
-                <span>By:  </span>
-                <span className="review-nickname">
-                    {review.details.nickname}
-                </span>
-            </div>
-        </div>
-        
+        </section>
+        <ReviewAside
+            location={ review.details.location }
+            nickname={ review.details.nickname }
+            created={ review.details.created_date }
+        />
+        <footer>
+            <div>Was this helpful (thumb up / down)</div>
+        </footer>
         <hr></hr>
-    </div>
+    </article>
 )
 
 export default Review;
