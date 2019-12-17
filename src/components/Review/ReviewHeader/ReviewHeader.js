c0c39ae3b539b0d667e05f6bf0f52049050c0cc2
Peter Eck <jpeckiii@gmail.com>
2019-12-16 18:17:18 -0600

    Extract header into separate component

---
+++ src/components/Review/ReviewHeader/ReviewHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './ReviewHeader.module.css'
+
+const ReviewHeader = ({ rating, headline }) => (
+    <header>
+        <div className={ styles.star_rating }>
+            <StarRating
+                rating={ rating }
+            />
+        </div>
+        <div className={ styles.review_headline }>
+            <strong>{ headline }</strong>
+        </div>
+    </header>
+);
+
+export default ReviewHeader;
