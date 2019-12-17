419f53754a747509f19cf0c6c770c71525e17e3b
Peter Eck <jpeckiii@gmail.com>
2019-12-16 18:17:42 -0600

    Replace header with ReviewHeader component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import styles from './Review.module.css';
 import StarRating from '../star-rating';
+import ReviewHeader from './ReviewHeader/ReviewHeader';
 import ReviewAside from './ReviewAside/ReviewAside';
 
 const Review = ({ review }) => (
     <article className={ styles.container } >
-        <header>
-            <div className={ styles.star_rating }>
-                <StarRating
-                    rating={ review.metrics.rating }
-                />
-            </div>
-            <div className={ styles.review_headline }>
-                <strong>{ review.details.headline }</strong>
-            </div>
-        </header>
+        <ReviewHeader
+            rating={ review.metrics.rating }
+            headline={ review.details.headline }
+        />
         <section class={ styles.review_comment }>
             <div className={ styles.review_comment }>
                 { review.details.comments }
