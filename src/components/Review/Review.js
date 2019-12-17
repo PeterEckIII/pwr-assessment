ecfbc47d86386c89959d8f618cdbc900bff32311
Peter Eck <jpeckiii@gmail.com>
2019-12-16 18:24:44 -0600

    Clean up components and styles

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './Review.module.css';
-import StarRating from '../star-rating';
 import ReviewHeader from './ReviewHeader/ReviewHeader';
 import ReviewAside from './ReviewAside/ReviewAside';
+import ReviewSection from './ReviewSection/ReviewSection';
+import ReviewFooter from './ReviewFooter/ReviewFooter';
 
 const Review = ({ review }) => (
     <article className={ styles.container } >
@@ -10,20 +11,15 @@ const Review = ({ review }) => (
             rating={ review.metrics.rating }
             headline={ review.details.headline }
         />
-        <section class={ styles.review_comment }>
-            <div className={ styles.review_comment }>
-                { review.details.comments }
-            </div>
-        </section>
+        <ReviewSection
+            comment={ review.details.comments }
+        />
         <ReviewAside
             location={ review.details.location }
             nickname={ review.details.nickname }
             created={ review.details.created_date }
         />
-        <footer>
-            <div>Was this helpful (thumb up / down)</div>
-        </footer>
-        <hr></hr>
+        <ReviewFooter />
     </article>
 )
 
