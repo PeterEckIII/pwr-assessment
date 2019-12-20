d141920adc4458ad27e3e12cf63a5ab6d0aea44c
Peter Eck <jpeckiii@gmail.com>
2019-12-20 10:22:18 -0600

    Restructure ReviewHeader

--- src/components/Review/ReviewHeader/ReviewHeader.js
+++ src/components/Review/ReviewHeader/ReviewHeader.js
@@ -3,12 +3,15 @@ import styles from './ReviewHeader.module.css'
 import StarRating from '../../star-rating';
 
 const ReviewHeader = ({ rating, headline }) => (
-    <header className={styles.container}>
+    <header className={styles.header}>
         <span className={ styles.star_rating }>
             <StarRating
                 rating={ rating }
             />
-        </span><span className={styles.rating_indicator}>{rating}</span>
+        </span>
+        <span className={styles.rating_indicator}>
+            {rating}
+        </span>
         <div className={ styles.review_headline }>
             <strong>{ headline }</strong>
         </div>
