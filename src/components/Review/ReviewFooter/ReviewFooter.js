69ca4e81f98da009f6f0ac47070c9fab78d204ea
Peter Eck <jpeckiii@gmail.com>
2019-12-20 10:20:38 -0600

    Add styles and helpful / not helpful votes

--- src/components/Review/ReviewFooter/ReviewFooter.js
+++ src/components/Review/ReviewFooter/ReviewFooter.js
@@ -5,9 +5,17 @@ import { ThumbIcon } from '../../thumb-icon';
 const ReviewFooter = ({ helpful, notHelpful }) => (
     <footer className={styles.footer}>
         <div className={styles.helpfulText}><strong>Was this review helpful to you?</strong></div>
-        <div className={styles.thumbIcon}>
-        <span><ThumbIcon /> { helpful }</span>
+        <div className={styles.helpfulVotes}>
+            <div className={styles.upThumbIcon}>
+                <ThumbIcon />
+                <span className={helpful}>{helpful}</span>
+            </div>
+            <div className={styles.downThumbIcon}>
+                <ThumbIcon />
+                <span className={styles.notHelpful}>{notHelpful}</span>
+            </div>
         </div>
+
     </footer>
 );
 
