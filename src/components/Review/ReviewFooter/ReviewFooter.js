f564d40c0bde4484a03721a5f0516ffdc112ae36
Peter Eck <jpeckiii@gmail.com>
2019-12-20 09:15:19 -0600

    Add scaffolding for helpful / not helpful votes

--- src/components/Review/ReviewFooter/ReviewFooter.js
+++ src/components/Review/ReviewFooter/ReviewFooter.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './ReviewFooter.module.css';
+import { ThumbIcon } from '../../thumb-icon';
 
-const ReviewFooter = () => (
+const ReviewFooter = ({ helpful, notHelpful }) => (
     <footer className={styles.footer}>
-        <div>Was this helpful (thumb up / down)</div>
+        <div className={styles.helpfulText}><strong>Was this review helpful to you?</strong></div>
+        <div className={styles.thumbIcon}>
+        <span><ThumbIcon /> { helpful }</span>
+        </div>
     </footer>
 );
 
