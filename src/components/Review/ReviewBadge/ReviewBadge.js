11fa6f125581c7ca56dae29d07bc14688224bba0
Peter Eck <jpeckiii@gmail.com>
2019-12-26 17:01:32 -0600

    Align Badge icon and Badge text

--- src/components/Review/ReviewBadge/ReviewBadge.js
+++ src/components/Review/ReviewBadge/ReviewBadge.js
@@ -4,34 +4,36 @@ import styles from './ReviewBadge.module.css';
 
 const ReviewBadge = ({ badges }) => {
     let badge;
-    if (badges.is_verified_buer) {
+    if (badges.is_verified_buyer) {
         badge = (
             <div className={ styles.badgeContainer }>
-                <span>
+                <div className={styles.badgeElement}>
                     <VerifiedBuyer />
-                    Verified Buyer
-                </span>
+                </div>
+                <span className={styles.badgeText}>Verified Buyer</span>
             </div>
         )
     } else if (badges.is_verified_reviewer) {
         badge = (
-            <div className={ styles.badgeContainer }>
-                <span>
+            <div className={styles.badgeContainer}>
+                <div className={styles.badgeElement}>
                     <VerifiedReviewer />
-                    Verified Reviewer
-                </span>
+                </div>
+                <span className={styles.badgeText}>Verified Review</span>
             </div>
         )
     } else if (badges.is_staff_reviewer) {
         badge = (
-            <div className={ styles.badgeContainer }>
-                <StaffReviewer />
-                <span>Staff Reviewer</span>
+            <div className={styles.badgeContainer}>
+                <div className={styles.badgeElement}>
+                    <StaffReviewer />
+                </div>
+                <span className={styles.badgeText}> Staff Reviewer</span>
             </div>
         )
     }
     return (
-        <div className={ styles.badgeElement }>
+        <div>
             { badge }
         </div>
     )
