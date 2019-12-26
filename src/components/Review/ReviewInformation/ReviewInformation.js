d0231f4263549f865dd1a7240a6a3be45d73f101
Peter Eck <jpeckiii@gmail.com>
2019-12-26 17:29:55 -0600

    Fix issue with relativeTime returning NaN

--- src/components/Review/ReviewInformation/ReviewInformation.js
+++ src/components/Review/ReviewInformation/ReviewInformation.js
@@ -2,28 +2,29 @@ import React from 'react';
 import styles from './ReviewInformation.module.css';
 import { relativeTime } from '../../../relative-time';
 import ReviewBadge from '../ReviewBadge/ReviewBadge';
-import classes from './ReviewInformation.module.css';
 
-const ReviewInformation = ({ location, nickname, created_date, badges }) => (
-    <aside className={ styles.information }>
-        <div>
-            <span><strong>Submitted: </strong>{ relativeTime(created_date) }</span>
-        </div>
+const ReviewInformation = ({ location, nickname, created, badges }) => {
+    return (
+        <aside className={ styles.information }>
+            <div>
+                <span><strong>Submitted: </strong>{ relativeTime(created) }</span>
+            </div>
 
-        <div>
-            <span><strong>By:  </strong></span>
-            <span>
-                { nickname }
-            </span>
-        </div>
-        <div>
-            <span><strong>From:  </strong></span>
-            <span>
-                { location }
-            </span>
-        </div>
-        <ReviewBadge badges={ badges } />
-    </aside>
-)
+            <div>
+                <span><strong>By:  </strong></span>
+                <span>
+                    { nickname }
+                </span>
+            </div>
+            <div>
+                <span><strong>From:  </strong></span>
+                <span>
+                    { location }
+                </span>
+            </div>
+            <ReviewBadge badges={ badges } />
+        </aside>
+    );
+}
 
 export default ReviewInformation;
