9c72a4b6f72b838f0c3d3a796546c87afc1cd8ba
Peter Eck <jpeckiii@gmail.com>
2019-12-29 11:35:58 -0600

    Move header to left / right side

--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -3,26 +3,26 @@ import Review from '../Review/Review';
 import styles from './ReviewList.module.css';
 import Filter from '../Filter/Filter';
 
-const ReviewList = ({ reviews }) => {
-    return (
-        <div>
-            <div className={styles.title}>
-                <span>Review Display</span>
-                <Filter />
-            </div>
-            <ul className={styles.list}>
-                { reviews.map(review => {
-                    return (
-                        <Review 
-                            key={review.review_id} 
-                            data-id={review.review_id}
-                            review={ review }
-                        />
-                    )
-                })}
-            </ul>
+const ReviewList = ({ reviews }) => (
+    <div>
+        <div className={styles.title}>
+            <span style={{ float: 'left' }}>Review Display</span>
         </div>
-    )
-}
+        <div className={styles.filterContainer}>
+            <Filter reviews={reviews} />
+        </div>
+        <ul className={styles.list}>
+            {reviews.map(review => {
+                return (
+                    <Review
+                        key={review.review_id}
+                        data-id={review.review_id}
+                        review={review}
+                    />
+                )
+            })}
+        </ul>
+    </div>
+)
 
 export default ReviewList;
