35be19c4d6fceb73d5ce8e75db89992b90749a25
Peter Eck <jpeckiii@gmail.com>
2019-12-31 11:52:24 -0600

    Move state and effects to ReviewList componnet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 import ReviewList from './components/ReviewList/ReviewList';
 
 const App = () => {
-    const [ reviews, setReviews ] = useState([]);
-
-    const fetchReviews = async () => {
-        try {
-            const result = await axios.get('/reviews');
-            setReviews(result.data.reviews);            
-        } catch (error) {
-            console.log(`Error: ${error}`)
-        }
-    }
-
-    useEffect(() => {    
-        fetchReviews()
-    }, [])
-
     return (
         <div>
-            <ReviewList reviews={reviews} />
+            <ReviewList />
         </div>
     )
 }
