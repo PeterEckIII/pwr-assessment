6dcc0174b98704e2eacb572bc1bfd4848eceb2db
Peter Eck <jpeckiii@gmail.com>
2020-01-03 12:06:20 -0600

    Move data fetching back to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import ReviewList from './components/ReviewList/ReviewList';
 
 const App = () => {
+    const [fetchedReviews, setFetchedReviews] = useState([]);
+    const fetchReviews = async () => {
+        try {
+            const result = await axios.get('/reviews');
+            setFetchedReviews(result.data.reviews);
+        } catch (error) {
+            console.log(`Error: ${error}`)
+        }
+    }
+    useEffect(() => {
+        fetchReviews();
+    })
+
     return (
         <div>
-            <ReviewList />
+            <ReviewList fetchedReviews={fetchedReviews} />
         </div>
     )
 }
