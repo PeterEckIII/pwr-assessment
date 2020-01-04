27b2bbea1062f4c4df097e5e4f5aa406d2c0890e
Peter Eck <jpeckiii@gmail.com>
2020-01-03 18:47:09 -0600

    Add onClick for thumb icons

--- src/components/Review/ReviewFooter/ReviewFooter.js
+++ src/components/Review/ReviewFooter/ReviewFooter.js
@@ -5,25 +5,34 @@ import { ThumbIcon } from '../../thumb-icon';
 const ReviewFooter = ({ helpful, notHelpful }) => {
     const [ helpfulVotes, setHelpfulVotes ] = useState(helpful)
     const [ unhelpfulVotes, setUnhelpfulVotes ] = useState(notHelpful);
+
     return (
         <footer className={styles.footer}>
             <div className={styles.helpfulText}>Was this review helpful to you?</div>
             <div className={styles.votes}>
                 
-                <div className={styles.thumbContainer}>
+                <div 
+                    className={styles.thumbContainer}
+                    onClick={() => setHelpfulVotes(v => v + 1)}
+                >
                     <div className={styles.upThumbIcon}>
                         <ThumbIcon />
                     </div>
                     &nbsp;
-                    <span className={styles.voteText}>{ helpful }</span>
+                    <span className={styles.voteText}>
+                        { helpfulVotes }
+                    </span>
                 </div>
 
-                <div className={styles.thumbContainer}>
+                <div
+                    className={styles.thumbContainer}
+                    onClick={() => setUnhelpfulVotes(v => v + 1)}
+                >
                     <div className={styles.downThumbIcon}>
                         <ThumbIcon />
                     </div>
                     &nbsp;
-                    <span className={styles.voteText}>{notHelpful}</span>
+                    <span className={styles.voteText}>{unhelpfulVotes}</span>
                 </div>
             </div>
         </footer>
